1b01e35e47d88e5205763e5b3053ee15ab5e5a73
fangqk1991 <me@fangqk.com>
2022-04-06 20:48:02 +0800

    v1.16.0: +VisitorInfo

--- src/common/kit/KitModels.ts
+++ src/common/kit/KitModels.ts
@@ -1,3 +1,11 @@
 export interface SimpleVisitor {
   email: string
 }
+
+export interface VisitorInfo extends SimpleVisitor {
+  iamId: number
+  email: string
+  name: string
+  permissionKeyMap: { [p: string]: 1 }
+  isAdmin?: boolean
+}
